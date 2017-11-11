3dea247155bc0bc58222a483a22f8baa65e08a66
Ida Bergström <Ida@Idas-MBP.zyxel.com>
2017-11-11 17:54:22 +0100

    test 7

--- partials/delete.php
+++ partials/delete.php
@@ -1,7 +1,6 @@
 <?php
 require "database.php";
 
-<<<<<<< HEAD
 foreach ($_POST as $key => $value){
    if(isset($_POST['delete'])){
        $statement = $pdo->prepare(
@@ -13,19 +12,3 @@ foreach ($_POST as $key => $value){
     
 ?>
 
-=======
-    $blog_id = $_POST["blog_id"];
-
-    $statement = $pdo->prepare("
-    DELETE FROM posts  
-     WHERE id = :blog_id
-      ");
-
-    $statement->execute(array(
-      ":blog_id" => $blog_id
-    )); 
-
-    var_dump($statement); 
-
-?>
->>>>>>> master
