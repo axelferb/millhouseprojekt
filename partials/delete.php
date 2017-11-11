8fabc8e20e0c15386c8f15789b718a97165f72be
Ida Bergström <Ida@Idas-MBP.zyxel.com>
2017-11-11 16:58:33 +0100

    Enskild användares inlägg listas med TA BORT-funktion

--- partials/delete.php
+++ partials/delete.php
@@ -1,17 +1,14 @@
 <?php
 require "database.php";
 
-    $blog_id = $_POST["blog_id"];
-
-    $statement = $pdo->prepare("
-    DELETE FROM posts  
-     WHERE id = :blog_id
-      ");
-
-    $statement->execute(array(
-      ":blog_id" => $blog_id
-    )); 
-
-    var_dump($statement); 
-
+foreach ($_POST as $key => $value){
+   if(isset($_POST['delete'])){
+       $statement = $pdo->prepare(
+           "DELETE FROM posts WHERE id = " . $value
+       );
+       $statement->execute();
+   }    
+}
+    
 ?>
+
