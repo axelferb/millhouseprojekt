88246f839e8766fa08ac209867c43033fd5a5293
ellinorjn <ellinor.jansson@medieinstitutet.se>
2017-11-14 14:00:07 +0100

    Nu funkar det jättebra tjoho

---
+++ registration_success.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    require 'head.php';
+    require 'partials/database.php';
+    require 'nav.php';
+?>
+
+<!--LOGIN FORM-->
+
+
+<div class="container mt-5">
+  <h4>Grattis! Du har nu registrerat dig och kan logga in:</h4>
+  
+  <form action="partials/login.php" method="POST">
+   
+    <div class="form-group">
+      <label for="username"> Username </label>
+      <input type="text" name="username" class="form-control">
+    </div>
+    
+    <div class="form-group">
+      <label for="password"> Password </label>
+      <input type="password" name="password" class="form-control">
+    </div>
+    
+    <div class="form-group">    
+      <input type="submit" class="btn btn-primary">
+    </div>
+    
+  </form>
+</div>
+
+
+<!--END OF LOGIN FORM-->
+
+
+    
+<?php require '../footer.php'; ?>
