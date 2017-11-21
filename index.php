c77950dfb802d273b3c2f62698a8c60437da367e
renemagitte <Ida@Idas-MacBook-Pro.local>
2017-11-21 11:48:04 +0100

    ny korrekt session

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-    session_start();
+// server should keep session data for AT LEAST 1 hour
+ini_set('session.gc_maxlifetime', 600);
+// each client should remember their session id for EXACTLY 1 hour
+session_set_cookie_params(600);
+session_start();
 ?>
 
 <!DOCTYPE html>
--- index_login.php
+++ index_login.php
@@ -1,24 +1,31 @@
 <?php
-//    session_start();
+
+// session_start();
     require 'partials/database.php';
-    // OM INLOGGNING SKETT FÖR ÖVER 10 MIN (10 * 60 sekunder = 600) SEDAN SKER AUTOMATISKT UTLOGGNING:
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
-    // last request was more than 30 minutes ago
-    session_unset();     // unset $_SESSION variable for the run-time 
-    session_destroy();   // destroy session data in storage
-    
-// ANNARS VISAS SIDAN I INLOGGAT LÄGE MED ANVÄNDARNAMN OCH LOG OUT-LÄNK:
-} else {  
-}
-
-$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 
 
-  
-// OM MAN ÅTERVÄNDER TILL INDEX EFTER ETT MISSLYCKAT REGISRERINGSFÖRSÖK:
-    if(isset($_GET["registration_error"])){
-        echo "Din registrering misslyckades då du inte fyllt i alla fält.";
-    }
+// DETTA ÄR GAMLA SESSION-GREJEN:
+//// OM INLOGGNING SKETT FÖR ÖVER 10 MIN (10 * 60 sekunder = 600) SEDAN SKER AUTOMATISKT UTLOGGNING:
+//if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
+//    // last request was more than 30 minutes ago
+//    session_unset();     // unset $_SESSION variable for the run-time 
+//    session_destroy();   // destroy session data in storage
+//    
+//// ANNARS VISAS SIDAN I INLOGGAT LÄGE MED ANVÄNDARNAMN OCH LOG OUT-LÄNK:
+//} else {  
+//}
+//
+//$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+//
+//
+//  
+//// OM MAN ÅTERVÄNDER TILL INDEX EFTER ETT MISSLYCKAT REGISRERINGSFÖRSÖK:
+//    if(isset($_GET["registration_error"])){
+//        echo "Din registrering misslyckades då du inte fyllt i alla fält.";
+//    }
+
+
+
 ?>
     <div class="hidden-xs hidden-sm col-md-4 login-wrap">
         <div class="login-field">
