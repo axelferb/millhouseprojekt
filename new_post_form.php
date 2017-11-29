4de17010644abe07edd44be7aa0b303d688d1e41
Niclas <niclas.victorsson@medieinstitutet.se>
2017-11-29 13:03:28 +0100

    updated style for new blogpost page and sized up usericon on profilepage

--- new_post_form.php
+++ new_post_form.php
@@ -9,86 +9,80 @@ require 'partials/session.php';
 require 'head.php';
 require 'partials/database.php';
 require 'partials/functions.php';       
-    
-    ?>
-    
+
+?>
+
 <body>
-    <?php
+
+  <?php
     require 'nav.php';
-    ?>
-    <div class="container">
+  ?>
 
+  <div class="container">
 
+    <div class="col-xs-12 col-md-12">
 
-        <div class="row">
-            <div class="col-xs-12 col-md-12">
-    
+      <h1>Nytt blogginlägg</h1>
 
-    
-<!--<div class="container mt-5">-->
-  <h4>Nytt inlägg:</h4>
-  
-  <form action="partials/new_post.php" method="POST" enctype="multipart/form-data">
-  
-    <div class="form-group">
-      <input type="hidden" name="user" value="<?= $_SESSION["user"]["id"]; ?> " class="form-control">
-    </div>
-  
-    <div class="form-group">
-      <label for="post_title"> Rubrik: </label>
-      <input type="text" name="post_title" class="form-control">
-    </div>
-    
-    <div class="form-group">
-      <label for="new_post"> Inlägg: </label>
-    <textarea name="new_post" id="editor"></textarea>
-    </div>
-    
-    <div class="form-group">
-      <label for="sel1">Select list:</label>
-      <select class="form-control" name="category" id="sel1">
-        <option>Solglasögon</option>
-        <option>Klockor</option>
-        <option>Inredning</option>
-      </select>
-    </div>
-  
-    <div class="form-group">
-      <label for="new_post"> Bild (valfritt): </label>
-       <input type="file" name="uploaded_file">
-    </div>
-   
-    
-    <div class="form-group">
-      <input type="submit" class="btn btn-primary">
-    </div>
-    
-  </form>
-<!--</div>-->
+      <hr class="full-width">
 
+      <form action="partials/new_post.php" method="POST" enctype="multipart/form-data">
 
-            </div>  
+        <div class="form-group">
+          <input type="hidden" name="user" value="<?= $_SESSION["user"]["id"]; ?> " class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="post_title"> Rubrik: </label>
+          <input type="text" name="post_title" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="new_post"> Inlägg: </label>
+          <textarea name="new_post" id="editor"></textarea>
+        </div>
+
+        <div class="form-group">
+          <label for="sel1">Kategori:</label>
+          <select class="form-control" name="category" id="sel1">
+            <option>Solglasögon</option>
+            <option>Klockor</option>
+            <option>Inredning</option>
+          </select>
+        </div>
+
+        <div class="form-group">
+          <label for="new_post">Bild (valfritt): </label>
+          <input type="file" name="uploaded_file">
         </div>
 
 
-    </div> <!-- END DIV / CONTAINER -->
+        <div class="form-group">
+          <input type="submit" class="btn btn-primary">
+        </div>
 
-    <?php
+      </form> 
+
+      <br><hr class="full-width"><br>
+
+      <a class="btn button-test btn-block" href="profilepage.php" target="_self">Tillbaka till profilsidan</a>
+
+    </div>
+
+  </div> <!-- END DIV / CONTAINER --> 
+
+  <?php
     require "footer.php";
-    ?>
-    
+  ?>
 
-
-
-<!--    Detta script är till wysiwyg editorn:-->
-    <script>
+  <!--    Detta script är till wysiwyg editorn:-->
+  <script>
     ClassicEditor
-        .create( document.querySelector( '#editor' ) )
-        .catch( error => {
-            console.error( error );
-        } );
-</script>
-
+      .create( document.querySelector( '#editor' ) )
+      .catch( error => {
+      console.error( error );
+    } );
+  </script>
 
 </body>
 
