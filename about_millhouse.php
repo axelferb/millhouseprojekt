065916d07d26ffd77b2b34f04fdfaf6dd6fc9202
ellinorjn <ellinor.jansson@medieinstitutet.se>
2017-12-06 16:48:45 +0100

    small changes and changed the name to english

---
+++ about_millhouse.php
@@ -0,0 +1,48 @@
+<?php
+require 'partials/session.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require 'head.php';
+require 'partials/database.php';
+require 'partials/functions.php';       
+?>
+
+    <body>
+
+    <?php
+    require 'nav.php';
+    ?>
+
+            <div class="jumbotron"></div>
+            <div class="blue-line"></div>
+            <div class="container">
+
+                    <main class="col-xs-12 col-md-12">
+                        <h1>Om Millhouse</h1>
+                        <hr class="full-width">
+                        <p>
+                           Millhouse är ett grossistföretag som säljer klockor, solglasögon och mindre inredningsartikar till mode- och livsstilsbutiker. Idag är vi på millhouse ett 50-tal anställda som främst jobbar inom administration, inköp och lagerhantering. Vi växer och utvecklas ständigt och söker därmed ofta nya medarbetare. Är du intresserad av att jobba hos oss så <a href="#">klicka här</a> för att se våra lediga tjänster eller för att skicka in din spontanansökan så återkommer vi till dig!
+                        </p> 
+                        
+                        <p> 
+                           Vi på Millhouse tar stor hänsyn till miljö och arbetsvillkor när vi väljer vilka produkter vi ska ta in. För oss är det viktigt att det vi säljer inte påverkar vår miljö på ett negativt sätt mer än nödvändigt. Vi väljer att köpa produkter där produktionen är så nära Sverige som möjligt för att få så korta transportsträckor som det går. Vi har ständigt noggranna granskningar av arbetsvillkoren, det är noll tolerans mot barnarbete och de som arbetar med att ta fram våra produkter ska ha bra arbetsvillkor hos sina arbetsgivare. Detta är något vi jobbar kontinuerligt med!
+                        </p>
+                        
+                        <p>   
+                           Nytt hos oss på Millhouse är vårt bloggsystem! Här kan du skapa inlägg innehållande önskemål av produkter, synpunkter och annat som faller dig in. Du kan även här läsa andra personers inlägg och förhoppningsvis få lite inspiration!
+                        </p>
+                        
+                          
+                    </div>
+            </div>
+            <!-- END DIV / CONTAINER -->
+
+            <?php
+    require "footer.php";
+    ?>
+
+    </body>
+
+</html>
