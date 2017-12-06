23c6ec80694d99f0668ed83e780e19717dc57bce
Mikael <micke@mikahl.se>
2017-12-06 19:44:48 +0100

    fixat jumbotron text

---
+++ jumbotron.php
@@ -0,0 +1,25 @@
+<div class="jumbotron">
+    
+      <div class="row">
+       <div class= "col-xs-8 col-xs-offset-3 col-sm-offset-4 mumma">
+            <div class= "col-xs-12 jumbo_text">
+           <p>SVERIGES BÄSTA BLOGG INOM</p>
+            </div>
+        </div>
+    </div> 
+    
+       <div class="row">   
+        <div class=" col-xs-8 col-xs-offset-3 col-sm-offset-4 category_wrapper">
+            <div class ="col-xs-push-2 col-xs-8 jumbo_klockor
+                         col-sm-offset-1 col-sm-push-0 col-sm-3"><p>KLOCKOR</p></div>
+            
+            <div class ="col-xs-push-2 col-xs-8 jumbo_sol
+                         col-sm-push-0 col-sm-4"><p>SOLGLASÖGON</p></div>
+                         
+            <div class ="col-xs-push-2 col-xs-8 jumbo_inredning
+                         col-sm-push-0 col-sm-3"><p>INREDNING</p></div>
+        </div>
+        
+    </div>
+    
+</div>
